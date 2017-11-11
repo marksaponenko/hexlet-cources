735e9192b388871e8a732a57010140a3ea9b2ca5
marksaponenko <forpokerstars.ru@yandex.ru>
2017-11-11 13:25:57 +0300

    add hexlet errors

--- errors-processing/fileSystem.js
+++ errors-processing/fileSystem.js
@@ -1,34 +1,21 @@
-/* Реализуйте следующие возможности файловой системы HexletFs:
-unlinkSync(path)
+/* Реализуйте функцию copySync(src, dest), которая копирует файл из src в dest.
 
-Удаляет файл (в реальной фс все чуть сложнее, см. hard link).
-
-Возможные ошибки:
-
-    ENOENT - файл не найден
-    EPERM - операция не разрешена. Такая ошибка возникает в случае, если path это директория
-
-writeFileSync(path, content)
-
-Записывает content в файл по пути path.
+    Если dest это путь до папки, то имя файла берется из src
+    Если dest это путь до файла (существующего или нет), то его содержимое становится равным src
 
 Возможные ошибки:
 
-    ENOENT - родительская директория, в которой нужно создать файл, не существует
-    EISDIR - path является директорией
-
-readFileSync(path)
-
-Читает содержимое файла по пути path.
-
-    ENOENT - файл не найден
-    EISDIR - path является директорией
+    EISDIR - возникает в случае, если src это директория, а не файл
+    ENOENT - возникает в случае, если src не существует, а так же возникает в случае,
+   если не существует директорий по пути dest (копирование не создает директорий)
 */
 
 import path from 'path';
 import errors from 'errno'; // eslint-disable-line
 import Tree from 'hexlet-trees'; // eslint-disable-line
 import { Dir, File } from 'hexlet-fs'; // eslint-disable-line
+
+import HexletFsError from './HexletFsError';
 
 const getPathParts = filepath =>
   filepath.split(path.sep).filter(part => part !== '');
@@ -41,94 +28,70 @@ export default class {
   statSync(filepath) {
     const current = this.findNode(filepath);
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
-    return [current.getMeta().getStats(), null];
+    return current.getMeta().getStats();
   }
 
-  unlinkSync(filepath) {
-    const { base, dir } = path.parse(filepath);
-    const parent = this.findNode(dir);
-
-    const current = this.findNode(filepath);
-
-    if (!current) {
-      return [null, errors.code.ENOENT];
+  copySync(src, dest) {
+    const node = this.findNode(dest);
+    const content = this.readFileSync(src);
+    const { base } = path.parse(src);
+    const resolvePath = `${dest}/${base}`;
+    if (node && this.statSync(dest).isDirectory()) {
+      return this.writeFileSync(resolvePath, content);
     }
-    if (current.getMeta().getStats().isDirectory()) {
-      return [null, errors.code.EPERM];
-    }
-
-    return [parent.removeChild(base), null];
+    return this.writeFileSync(dest, content);
   }
 
-  writeFileSync(filepath, content) {
-    const { base, dir } = path.parse(filepath);
+  writeFileSync(filepath, body) {
+    const { dir, base } = path.parse(filepath);
     const parent = this.findNode(dir);
-    const current = this.findNode(filepath);
-
     if (!parent) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
-    if (current && current.getMeta().getStats().isDirectory()) {
-      return [null, errors.code.EISDIR];
+    const current = parent.getChild(base);
+    if (current && current.getMeta().isDirectory()) {
+      throw new HexletFsError(errors.code.EISDIR, filepath);
     }
-    return [parent.addChild(base, new File(base, content)), null];
+    parent.addChild(base, new File(base, body));
+  }
+
+  touchSync(filepath) {
+    const { dir, base } = path.parse(filepath);
+    const parent = this.findNode(dir);
+    if (!parent) {
+      throw new HexletFsError(errors.code.ENOENT, filepath);
+    }
+    if (parent.getMeta().isFile()) {
+      throw new HexletFsError(errors.code.ENOTDIR, filepath);
+    }
+    return parent.addChild(base, new File(base, ''));
+  }
+
+  mkdirpSync(filepath) {
+    getPathParts(filepath).reduce((subtree, part) => {
+      const current = subtree.getChild(part);
+      if (!current) {
+        return subtree.addChild(part, new Dir(part));
+      }
+      if (current.getMeta().isFile()) {
+        throw new HexletFsError(errors.code.ENOTDIR, filepath);
+      }
+
+      return current;
+    }, this.tree);
   }
 
   readFileSync(filepath) {
     const current = this.findNode(filepath);
-
     if (!current) {
-      return [null, errors.code.ENOENT];
+      throw new HexletFsError(errors.code.ENOENT, filepath);
     }
-    if (current.getMeta().getStats().isDirectory()) {
-      return [null, errors.code.EISDIR];
+    if (current.getMeta().isDirectory()) {
+      throw new HexletFsError(errors.code.EISDIR, filepath);
     }
-
-    return [current.getMeta().getBody(), null];
-  }
-
-  mkdirpSync(filepath) {
-    const iter = (parts, subtree) => {
-      if (parts.length === 0) {
-        return [subtree, null];
-      }
-      const [part, ...rest] = parts;
-      const current = subtree.getChild(part);
-      if (!current) {
-        return iter(rest, subtree.addChild(part, new Dir(part)));
-      }
-      if (current.getMeta().isFile()) {
-        return [null, errors.code.ENOTDIR];
-      }
-
-      return iter(rest, current);
-    };
-    const parts = getPathParts(filepath);
-    return iter(parts, this.tree);
-  }
-
-  touchSync(filepath) {
-    const { base, dir } = path.parse(filepath);
-    const parent = this.findNode(dir);
-    if (!parent) {
-      return [null, errors.code.ENOENT];
-    }
-    if (parent.getMeta().isFile()) {
-      return [null, errors.code.ENOTDIR];
-    }
-    return [parent.addChild(base, new File(base, '')), null];
-  }
-
-  readdirSync(filepath) {
-    const dir = this.findNode(filepath);
-    if (!dir) {
-      return [null, errors.code.ENOENT];
-    } else if (dir.getMeta().isFile()) {
-      return [null, errors.code.ENOTDIR];
-    }
-    return [dir.getChildren().map(child => child.getKey()), null];
+    return current.getMeta().getBody();
   }
 
   findNode(filepath) {
